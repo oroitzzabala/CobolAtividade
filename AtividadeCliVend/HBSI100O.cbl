49ace0d1e1a96543dd615f34b7c55e230ce43f41
tadeu <tadeu@DEATHWING>
2020-03-24 12:14:24 -0300

    Primeiro commit para o github.

---
+++ AtividadeCliVend/HBSI100O.cbl
@@ -0,0 +1,15 @@
+       program-id. HBSI100O as "HBSI100O".
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+
+       procedure division.
+
+           display "Ola Github"
+
+           goback.
+
+       end program HBSI100O.
